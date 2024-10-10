25f7aa0b4f2a92cce8ab369281921dac970fd423
bader <bader5uk@hotmail.com>
2024-10-10 09:32:09 +0400

    Refactored and cleaned code

--- UnionOfSets.java
+++ UnionOfSets.java
@@ -4,17 +4,15 @@ public class UnionOfSets {
     /*
     Adds two sets together
      */
-    static List<Integer> array1 = Arrays.asList(1,2,3,4,5);
-    static List<Integer> array2 = Arrays.asList(6,7,8,9,10);
-    static Set<Integer> setA = new HashSet<>(array1);
-    static Set<Integer> setB = new HashSet<>(array2);
+    static Set<Integer> setA = new HashSet<>(Arrays.asList(1,2,3,4,5));
+    static Set<Integer> setB = new HashSet<>(Arrays.asList(6,7,8,9,10));
 
     public static void main(String[]args){
-        union();
+        union(setA,setB);
     }
-    public static void union(){
-        Set<Integer> intersection = new HashSet<>(setA);
-        intersection.addAll(setB);
+    public static void union(Set<Integer> set1, Set<Integer> set2 ){
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.addAll(set2);
         System.out.println("The union of the two sets is:  " + intersection);
     }
 }
