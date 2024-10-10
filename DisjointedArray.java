2b1fde2cd280849c76e03b308888ecb41ee33d44
bader <bader5uk@hotmail.com>
2024-10-10 10:00:05 +0400

    Refactored and cleaned code

--- DisjointedArray.java
+++ DisjointedArray.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class DisjointedArray {
@@ -11,15 +10,29 @@ public class DisjointedArray {
              - convert it into a Set.
              - compare if set 1 is equals to set 2
      */
-    static List<Integer> List1 = Arrays.asList(1,2,3,4,5,6,7);
-    static List<Integer> List2 = Arrays.asList(1,2,3,4,5,6,7);
-    static Set<Integer> numberSet1= new HashSet<>(List1);
-    static Set<Integer> numberSet2= new HashSet<>(List2);
+    static Set<Integer> numberSet1= new HashSet<>(Arrays.asList(3,2,1));
+    static Set<Integer> numberSet2= new HashSet<>(Arrays.asList(1,2,3));
     public static void main(String[]args){
-        Boolean checker = true;
-        if(numberSet1.equals(numberSet2)){
-            checker = false;
+        Boolean checker = false;
+
+        Set<Integer> intersection = new HashSet<>(numberSet1);
+        intersection.retainAll(numberSet2);
+
+        for (Integer x : numberSet1){
+            if(numberSet2.contains(x)){
+                checker = false;
+            }
+            else {
+                checker = true;
+            }
         }
-        System.out.println("Are the two arrays Disjointed?  " + checker);
+        if(checker = true){
+            System.out.println("The repeating elements are: " + intersection);
+        }
+        else {
+            System.out.println("The two arrays are disjointed");
+
+        }
+
     }
 }
